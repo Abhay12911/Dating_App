e4f0904f25a20488366908e0733c6484351a5af8
Abhay12911 <abhaystp30@gmail.com>
2024-12-08 10:38:13 +0530

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className="bg-[url('https://www.thelist.com/img/gallery/dating-app-bios-to-make-your-profile-stand-out/intro-1677707823.webp')] bg-cover bg-center h-screen w-full">HomePage</div>
+  )
+}
+
+export default HomePage
